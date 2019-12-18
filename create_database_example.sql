58487f7e1c978965e1d98f374169b4abde62de42
mtbrother <mtbrothers@outlook.com>
2019-12-18 21:54:23 +0500

    удалил

--- create_database_example.sql
+++
@@ -1,7 +0,0 @@
-use example;
-drop table if exists users;
-create table users (
- id SERIAL primary key,
- name varcharacter(255) unique);
- 
-describe users;
--- create_dump_database.sql
+++
@@ -1,6 +0,0 @@
-drop database if exists sample;
-create database sample;
-mysqldump example > example.sql;
-mysql sample < example.sql;
-use sample;
-describe users;
